21bb518a01a8361febe6506433352fdd8475529f
muhammaddanielafifi <254107020206@student.polinema.ac.id>
2025-10-01 14:05:07 +0700

    tugas3

---
+++ jobsheet5/tugas3PerpustakaanWifi.java
@@ -0,0 +1,21 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class tugas3PerpustakaanWifi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("apakah membawa kartu mahasiswa? (true/false): ");
+        boolean kartuMhs = scanner.nextBoolean();
+
+        System.out.print("apakah sudah melakukan registrasi online? (true/false): ");
+        boolean registrasiOnline = scanner.nextBoolean();
+        
+        if (kartuMhs || registrasiOnline){
+            System.out.println("Boleh masuk");
+        }else{
+            System.out.println("Ditolak masuk");
+        }
+    }
+}
